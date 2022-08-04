37083184f6bfb28290065c005f4db0354ac5f8cb
vaishak <vaishak7026@gmail.com>
2022-08-04 17:41:45 +0530

    database creation

--- my_project/blog/models.py
+++ my_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Medicals(models.Model):
+    Name = models.TextField(max_length=200, blank=False)
+    Number = models.IntegerField(max_length=100, blank=False)
+    Email_id= models.EmailField()
+    Message=models.CharField(max_length=300, blank=False)
+    photo = models.ImageField()
+
+
+    def __str__(self):
+        return self.Name
+
