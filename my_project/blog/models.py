ffab469dc092bd7d6de18953f32479dbf41a511d
RaswanthGS <raswanth47@gmail.com>
2022-08-04 17:53:25 +0530

    Update

--- my_project/blog/models.py
+++ my_project/blog/models.py
@@ -5,7 +5,6 @@ class Medicals(models.Model):
     Number = models.IntegerField(blank=False)
     Email_id= models.EmailField()
     Message=models.CharField(max_length=300, blank=False)
-    photo = models.ImageField()
 
 
     def __str__(self):
