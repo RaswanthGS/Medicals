91094facc1f75d37828a429a4febb9874b4fdfde
RaswanthGS <raswanth47@gmail.com>
2022-08-06 12:23:12 +0530

    Update

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,18 +1,5 @@
-from django.shortcuts import render, redirect
-from .forms import PostForm
+from django.shortcuts import render
 
 def home(request):
 
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save() 
-            return redirect('/')
-    else:
-        form = PostForm()
-
-    context = {
-        'forms':form
-    }
-
-    return render(request, 'blog/base.html', context)
+    return render(request, 'blog/base.html')
