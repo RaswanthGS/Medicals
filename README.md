0a19352bc6efda4daca9e314bbc60d320621de57
RaswanthGS <raswanth47@gmail.com>
2022-08-10 16:47:05 +0530

    Update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Medicals
- 
+# Medicals-2
--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,3 @@
-from dataclasses import field
-from socket import fromshare
 from django import forms
 from django.forms import ModelForm
 from .models import Medicals
